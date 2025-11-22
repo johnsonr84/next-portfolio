9f82eace8e53bd0ffecf10a1cab4accbcb34487c
Dev Dork <robertcjohnson1984@gmail.com>
2025-11-21 22:45:28 -0700

    add component

---
+++ src/components/PortfolioContent.tsx
@@ -0,0 +1,15 @@
+import {
+  AboutSection,
+  HeroSection,
+} from "@/components/sections";
+
+async function PortfolioContent() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+    </>
+  );
+}
+
+export default PortfolioContent;
