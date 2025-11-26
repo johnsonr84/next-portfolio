93e851ee0c9547059d150d65fc251651ee32afbd
Dev Dork <robertcjohnson1984@gmail.com>
2025-11-25 18:10:15 -0700

    revert to previous

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,10 +1,9 @@
-// import PortfolioContent from "../../components/PortfolioContent";
+import PortfolioContent from "../../components/PortfolioContent";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Deployed test page</h1>
-      <p>If you see this on Vercel, the routing is fine.</p>
+    <main className="min-h-screen">
+      <PortfolioContent />
     </main>
   );
 }
